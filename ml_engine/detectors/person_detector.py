c89dd044cc456c5fc027f59ea2617c74c2003b70
sparsh-kapil08 <sparshkapil2008@gmail.com>
2026-02-07 16:13:09 +0000

    render changes

--- ml_engine/detectors/person_detector.py
+++ ml_engine/detectors/person_detector.py
@@ -1,9 +1,9 @@
 import cv2
-import mediapipe as mp
-print(f"DEBUG: MediaPipe path is {mp.__file__}")
-mp_pose = mp.solutions.pose.Pose()
+from mediapipe.python.solutions import pose as mp_pose
+
+pose = mp_pose.Pose()
 
 def detect_person(frame):
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    result = mp_pose.process(rgb)
+    result = pose.process(rgb)
     return result.pose_landmarks is not None
